d0975d9a905e1731f30f86c10ad80702f69c18ce
Pglagarto2005 <santiblo05@gmail.com>
2026-03-12 10:44:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 
 
 st.title('Análisis de Sentimiento')
-image = Image.open('emoticones.jpg')
+image = Image.open('higuruma.jpg')
 st.image(image)
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
 
